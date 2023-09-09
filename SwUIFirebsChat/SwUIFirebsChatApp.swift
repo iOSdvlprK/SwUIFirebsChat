597000a010996605fc4de32417e414237a361809
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-09-09 21:44:17 +0900

    Initial Commit

---
+++ SwUIFirebsChat/SwUIFirebsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUIFirebsChatApp.swift
+//  SwUIFirebsChat
+//
+//  Created by joe on 2023/09/09.
+//
+
+import SwiftUI
+
+@main
+struct SwUIFirebsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
